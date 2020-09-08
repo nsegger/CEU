213fe028d6658ebf46685a150707a25274cfc96f
Nicholas Segger <painn5@yahoo.com>
2020-09-07 23:14:33 -0300

    refactor: Remove ListSelectionListener

--- com/src/screens/common/StocksJList.java
+++ com/src/screens/common/StocksJList.java
@@ -1,10 +1,6 @@
 package screens.common;
 
-import screens.stocks.Stocks;
-
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -26,7 +22,6 @@ public class StocksJList extends JList<String> implements MouseMotionListener {
         this.hoveredColor = hoveredColor;
         this.renderer = renderer;
 
-
         setCellRenderer(renderer);
         setSelectionBackground(new Color(36,36,36));
         setFixedCellHeight(45);
@@ -37,12 +32,6 @@ public class StocksJList extends JList<String> implements MouseMotionListener {
             public void mouseExited(MouseEvent e) {
                 renderer.setHoveredIndex(-1);
                 repaint();
-            }
-        });
-
-        addListSelectionListener(e -> {
-            if (!e.getValueIsAdjusting()) {
-                System.out.println("Selected value is now: " + getSelectedValue());
             }
         });
     }
