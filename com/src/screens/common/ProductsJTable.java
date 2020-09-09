3b7e77d3edfed402ae4d00c87ccae4059078e2fd
Nicholas Segger <painn5@yahoo.com>
2020-09-08 23:09:59 -0300

    feat: Add common ProductsJTable and ProductsTableCellRenderer components

---
+++ com/src/screens/common/ProductsJTable.java
@@ -0,0 +1,66 @@
+package screens.common;
+
+import framework.Logger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class ProductsJTable extends JTable implements MouseMotionListener {
+    private Color hoveredColor;
+    ProductsTableCellRenderer renderer;
+
+    public ProductsJTable() {
+        this(new ProductsTableCellRenderer());
+    }
+
+    public ProductsJTable(ProductsTableCellRenderer renderer) {
+        this(renderer, new Color(255, 255, 255));
+    }
+
+    public ProductsJTable(ProductsTableCellRenderer renderer, Color hoveredColor) {
+        this.hoveredColor = hoveredColor;
+        this.renderer = renderer;
+
+        setDefaultRenderer(JLabel.class, renderer);
+        setSelectionBackground(new Color(36,36,36));
+        setRowHeight(45);
+
+        addMouseMotionListener(this);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                Logger.info("Mouse exited JTable");
+                renderer.setHoveredRow(-1);
+                repaint();
+            }
+        });
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        isItemBeingHovered(e);
+    }
+
+    private void isItemBeingHovered(MouseEvent e) {
+        int row = rowAtPoint(e.getPoint());
+
+        if (row != getSelectedRow() && row != renderer.getHoveredRow()) {
+            renderer.setHoveredRow(row);
+            repaint();
+        }
+    }
+
+    public void setHoveredColor(Color hoveredColor) {
+        this.hoveredColor = hoveredColor;
+    }
+
+    public Color getHoveredColor() {
+        return hoveredColor;
+    }
+}
