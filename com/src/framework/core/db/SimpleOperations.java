53be11b13effe20a520de69229a5ff0aaaae5185
Nicholas Segger <painn5@yahoo.com>
2020-09-10 01:38:10 -0300

    refactor: Add findByAnd method

--- com/src/framework/core/db/SimpleOperations.java
+++ com/src/framework/core/db/SimpleOperations.java
@@ -2,10 +2,12 @@ package framework.core.db;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public interface SimpleOperations<T extends Model<T> > {
     List<T> findAll() throws SQLException;
     T findBy(DatabaseColumn column) throws SQLException;
+    T findByAnd(Map<String, Object> column) throws SQLException;
     T findById(int id) throws SQLException;
     boolean create(T entry) throws SQLException;
 }
