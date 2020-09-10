ac22f47ec4f2f24f014cebe3227e3aca70520fb6
Nicholas Segger <painn5@yahoo.com>
2020-09-10 18:46:20 -0300

    feat: Add patch method

--- com/src/app/product/ProductInterface.java
+++ com/src/app/product/ProductInterface.java
@@ -6,12 +6,13 @@ import framework.core.db.DatabaseInterface;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductInterface extends DatabaseInterface<Product> {
     public ProductInterface(Connection connection) {
         super(connection, Product.class);
-        table = "stocks";
+        table = "products";
     }
 
     public List<Product> index(int stockId) {
@@ -24,20 +25,47 @@ public class ProductInterface extends DatabaseInterface<Product> {
             throwables.printStackTrace();
         }
 
-        return null;
+        return new ArrayList<>();
     }
 
-    public boolean store(String name, int stockId) {
-        Logger.info("Criando produto " + name + " pertencente ao estoque de id " + stockId);
+    public boolean store(Product product) {
+        Logger.info("Criando produto " + product.getName() + " pertencente ao estoque de id " + product.getStockId());
 
         try {
-            return create(new Product(name, 1, stockId));
+            return create(product);
         } catch (SQLException throwables) {
             Logger.error("Erro ao criar produto:");
             throwables.printStackTrace();
         }
 
         return false;
+    }
+
+    public boolean patch(Product product) {
+        Logger.info("Editando produto " + product.getName());
+
+        try {
+            return update(product);
+        } catch (SQLException throwables) {
+            Logger.info("Erro ao editar produto:");
+            throwables.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean update(Product product) throws SQLException {
+        String name = product.getName();
+        int amount = product.getAmount();
+
+        Logger.db("Atualizando produto [" + product.getId() + "] " + name);
+
+        String query = String.format("UPDATE %s SET %s WHERE %s", table, "name = '" + name + "', amount = '" + amount + "'", product.toWhere());
+
+        int rowsAffected = connection.prepareStatement(query).executeUpdate();
+        Logger.db("Executado update \"" + query + "\" com " + rowsAffected + " linhas afetadas");
+
+        return rowsAffected > 0;
     }
 
     public boolean remove(Product entry) {
